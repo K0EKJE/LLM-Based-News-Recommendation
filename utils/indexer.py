c92e1698aa4aa56c78ea3de9363f3fbbf8350e80
Haoran <jhr12578@g.ucla.edu>
2023-12-14 13:15:41 -0800

    docstrings added

--- utils/indexer.py
+++ utils/indexer.py
@@ -3,8 +3,17 @@ import numpy as np
 import faiss
 
 
-
 def get_index():
+        """
+        Create and return a Faiss index based on embeddings stored in 'dataset/embeddings.npy'
+
+        Parameters:
+        None
+
+        Returns:
+        faiss.IndexFlatL2: a Faiss flat L2 index containing the loaded embeddings
+        """
+
         # Check if the embeddings file exists
         if os.path.exists('dataset/embeddings.npy'):
             # Load the embeddings
@@ -12,13 +21,30 @@ def get_index():
         else:
             print('No existing embeddings found')
 
-        # Create a Faiss index - here we use a simple flat L2 index
+        # Create a Faiss index (a simple flat L2 index)
         index = faiss.IndexFlatL2(embeddings.shape[1])
 
         # Add vectors to the index
         index.add(embeddings)
-        
+
         return index
+
+def similarity_search(vec, k, index):
+        """
+        Perform similarity search on a Faiss index
+
+        Parameters:
+        - vec (numpy.ndarray): the vector for which to search similar vectors
+        - k (int): the number of nearest neighbors to retrieve
+        - index (faiss.IndexFlatL2): the Faiss index to search
+
+        Returns:
+        tuple: a tuple containing two arrays, which are D (distances) and I (indices) of the k nearest neighbors
+        """
+
+        # Search the index (D is distance, I is index of neighbors)
+        D, I = index.search(vec, k)
+        return D, I
 
 def index_dataset(df):
         # Check if the embeddings file exists
@@ -35,11 +61,5 @@ def index_dataset(df):
 
         # Add vectors to the index
         index.add(embeddings)
-        
-        return index
-        
-def similarity_search(vec, k, index):
 
-        # Search the index
-        D, I = index.search(vec, k)  # D is distance, I is index of neighbors
-        return D, I
+        return index
--- utils/util.py
+++ utils/util.py
@@ -42,7 +42,19 @@ def partition_large_dataset(file_path, output_folder_path):
 
 
 def upload_files_to_s3(bucket_name, folder_path, aws_access_key_id, aws_secret_access_key):
-        '''upload a folder to s3'''
+        """
+        Upload all files from a local folder to an Amazon S3 bucket
+
+        Parameters:
+        - bucket_name (str): the name of the S3 bucket
+        - folder_path (str): the local path of the folder containing files to be uploaded
+        - aws_access_key_id (str): AWS access key ID with S3 written permissions
+        - aws_secret_access_key (str): AWS secret access key corresponding to the provided access key
+
+        Returns:
+        None
+        """
+
         # Create an S3 client
         s3 = boto3.client(
             's3',
@@ -62,6 +74,21 @@ def upload_files_to_s3(bucket_name, folder_path, aws_access_key_id, aws_secret_a
 
 
 def read_from_partitions(bucket_name, base_file_path, target_indices, partition_size, aws_access_key_id, aws_secret_access_key):
+        """
+        Read specific rows from partitioned CSV files stored in an Amazon S3 bucket and combine them into a DataFrame
+
+        Parameters:
+        - bucket_name (str): the name of the S3 bucket
+        - base_file_path (str): the base path of partitioned CSV files without the partition suffix
+        - target_indices (list): a list of row indices to retrieve from the partitions
+        - partition_size (int): the size of each partition
+        - aws_access_key_id (str): AWS access key ID with S3 written permissions
+        - aws_secret_access_key (str): AWS secret access key corresponding to the provided access key
+
+        Returns:
+        pandas.DataFrame: a DataFrame containing the rows specified by the target indices
+        """
+
         # Create S3 client
         s3 = boto3.client(
             's3',
@@ -76,11 +103,11 @@ def read_from_partitions(bucket_name, base_file_path, target_indices, partition_
         rows = []
 
         for index in target_indices:
-
+            # Calculate the partition suffix
             partition_number = index // partition_size + 1
-            if partition_number not in partition_dfs:
 
-                # Calculate the partition file name
+            if partition_number not in partition_dfs:
+                # Obtain the partition file name
                 partition_file_name = f"{base_file_path}_part_{partition_number}.csv"
 
                 # Get the object from S3
@@ -96,14 +123,22 @@ def read_from_partitions(bucket_name, base_file_path, target_indices, partition_
 
             # Retrieve the row by index if it exists in the DataFrame
             if index in list(df.Index):
-
                 rows.append(df[df.Index == index])
 
         # Combine all rows into a single DataFrame
         return pd.concat(rows, axis=0)
 
 def read_from_local_partitions(target_indices, partition_size):
+        """
+        Read specific rows from partitioned CSV files stored locally and combine them into a DataFrame
 
+        Parameters:
+        - target_indices (list): a list of row indices to retrieve from the partitions
+        - partition_size (int): the size of each partition
+
+        Returns:
+        pandas.DataFrame: a DataFrame containing the rows specified by the target indices
+        """
         base_file_path = "NYTimes"
         # Dictionary to hold dataframes for each partition
         partition_dfs = {}
